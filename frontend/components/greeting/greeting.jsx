02d33bbdf6220a6ee5486ca36c861dcd9ba83e3c
your name here <werock2000@gmail.com>
2020-05-03 22:56:56 -0700

    Modify greeting page

--- frontend/components/greeting/greeting.jsx
+++ frontend/components/greeting/greeting.jsx
@@ -10,8 +10,6 @@ const Greeting = ({ currentUser, logout,ownprops }) => {
         {/* <Link to="/newgroup" className="toggle_group">Create a group</Link> */}
         <Link to="/login" className="toggle_btnin">Log in</Link>
         <Link to="/signup" className="toggle_btnup" >Sign up</Link>
-     
-      {/* <img src="https://cdn2us.denofgeek.com/sites/denofgeekus/files/styles/main_wide/public/2020/01/lord_of_the_rings_gollum.jpg?itok=-ndMZJ1B" alt=""/> */}
       </div>
     </div>
   );
