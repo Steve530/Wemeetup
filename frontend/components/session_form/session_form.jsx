0280cc861e3c41ad275ff2e78a553123a1096f24
your name here <werock2000@gmail.com>
2020-06-21 00:00:24 -0700

    modify login  page

--- frontend/components/session_form/session_form.jsx
+++ frontend/components/session_form/session_form.jsx
@@ -6,7 +6,7 @@ class SessionForm extends React.Component {
     this.state = {
       username: '',
       password: ''
-    };  
+    };    
     this.handleSubmit = this.handleSubmit.bind(this);
     this.demouser = this.demouser.bind(this);
   }
