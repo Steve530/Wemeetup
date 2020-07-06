75dda8a96967bb8bca0100b2e4eaef160a2ef660
your name here <werock2000@gmail.com>
2020-07-06 00:19:27 -0700

    modify greeting  page

--- frontend/components/group/afterlogin_index_item.jsx
+++ frontend/components/group/afterlogin_index_item.jsx
@@ -20,7 +20,7 @@ class AfterloginIndexItem extends React.Component {
       <div className="group-index-item"  onClick={this.handleClick}>
         <div className={this.props.type==="yourgroups" ? "group-info-yours" : "group-info2"}>
             <img src={`./${idd}.jpg`} />
-          <div className="group-info-text">
+          <div className="group-info-text"> 
             <h4 id="group-item-name">{(this.props.group.group_name.length <= 19 ) ? this.props.group.group_name : this.props.group.group_name.slice(0, 18)+"..." }</h4>
             <p id="num-members">{this.props.group.membersarray} {(this.props.group.membersarray === 1) ? "member" : "members"}</p>
     
