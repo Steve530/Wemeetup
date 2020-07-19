370227999fe868180011bd247d96ff0d77e7f022
your name here <werock2000@gmail.com>
2020-07-18 21:38:42 -0700

    modify afterlogin  page

--- frontend/components/group/afterlogin_index.jsx
+++ frontend/components/group/afterlogin_index.jsx
@@ -7,7 +7,7 @@ import bxSearch from '@iconify/icons-bx/bx-search';
 class AfterloginIndex extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {  
+    this.state = {    
       search: ''
     }; 
     this.yesmenu = this.yesmenu.bind(this);
@@ -40,45 +40,41 @@ nomenu() {
    
   return( 
     <div className='afterlogin-groupandpics'>
-
-      <section id="member-home-header"> 
-          <div id="member-home-bounds">
-                <h2 className="heroPrimary">Find your next event</h2>
-                  <p >  
-                    <span className="heroSecondary">
-                    174 events in your groups  . 1,295 events near you  
-                    </span> 
-                  </p> 
-                 
-          </div> 
-      </section> 
-      <div className="all-groups-images">
-            
+        <section id="member-home-header"> 
+            <div id="member-home-bounds">
+                  <h2 className="heroPrimary">Find your next event</h2>
+                    <p >  
+                      <span className="heroSecondary">
+                      174 events in your groups  . 1,295 events near you  
+                      </span> 
+                    </p> 
+            </div> 
+        </section> 
+          <div className="all-groups-images">
             <h2 id='afterlogin-urgroups'>YOUR GROUPS</h2>
-            <div className='your_groups'>
+             <div className='your_groups'>
                   <div className="search_container" >
-                    <input id="search_input" type="text" value={this.state.search} onChange={this.update('search')} onClick={this.yesmenu}/>
-                    <div id="search_caption"  > within  <a className="search_caption2" href=""> 5 miles</a> of  <a className="search_caption2" href="">  San Francisco, CA</a></div>
-                  <div className="search_icon">
-                    <Icon width="25" height="25" icon={bxSearch} />
-                  </div> 
-                 
+                      <input id="search_input" type="text" value={this.state.search} onChange={this.update('search')} onClick={this.yesmenu}/>
+                         <div id="search_caption"  > within<a className="search_caption2" href=""> 5 miles</a> of<a className="search_caption2" href="">  San Francisco, CA</a></div>
+                                <div className="search_icon">
+                                  <Icon width="25" height="25" icon={bxSearch} />
+                                </div> 
                   </div>
-                  <div className="dropdown_allgroups"> 
-                     {this.state.yesmenu ? (
-                        <div className="dropdown_allgroups2">
-                            <ul className="dropdown_groups3">{this.props.groups.slice(0,25).map((group)=> 
-                                 <ul className="separate_groupname3">
-                                   <Link to={`/groups/${group.id}`} className="dropdown_groupnames">{group.group_name}</Link>
-  
-                                  </ul>
-                            )}</ul>
-                        </div>
-                     ) : ( null )
-                     }
-            </div>
-              { this.props.groups.filter(group => group.members.includes(this.props.currentUser_id) ).map(group=> 
-              <AfterloginIndexItem type="yourgroups" key = {group.id} group={group}/>)}
+                    <div className="dropdown_allgroups"> 
+                       {this.state.yesmenu ? (
+                            <div className="dropdown_allgroups2">
+                                <ul className="dropdown_groups3">{this.props.groups.slice(0,25).map((group)=> 
+                                    <ul className="separate_groupname3">
+                                      <Link to={`/groups/${group.id}`} className="dropdown_groupnames">{group.group_name}</Link>
+      
+                                      </ul>
+                                )}</ul>
+                            </div>
+                        ) : ( null )
+                        }
+                     </div>
+                 { this.props.groups.filter(group => group.members.includes(this.props.currentUser_id) ).map(group=> 
+                <AfterloginIndexItem type="yourgroups" key = {group.id} group={group}/>)}
             </div>
             <hr className="afterlogin-border"/>
          
