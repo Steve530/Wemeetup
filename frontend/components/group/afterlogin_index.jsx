a9a499926b7fbec1bfde49eeb859407df3665735
your name here <werock2000@gmail.com>
2020-07-26 01:23:06 -0700

    modify afterlogin  page

--- frontend/components/group/afterlogin_index.jsx
+++ frontend/components/group/afterlogin_index.jsx
@@ -95,7 +95,7 @@ nomenu() {
                                           <ul className="dropdown_part3">{this.props.groups.slice(15,22).map((group)=> 
                                               <ul className="separate_groupname3">
                                                 <Link to={`/groups/${group.id}`} className="dropdown_groupnames">{group.group_name}</Link>
-                                              </ul> 
+                                              </ul>     
                                           )}</ul>
                                       </div>
                                       <div>
